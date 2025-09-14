0b4e15d58da76ca88048b0904b5beff4b5c22cd0
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-13 21:48:10 -0300

    feat: update output values to use bucket_name for consistency

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "bronze_bucket" { value = module.bronze_bucket.name }
-output "silver_bucket" { value = module.silver_bucket.name }
-output "gold_bucket"   { value = module.gold_bucket.name }
+output "bronze_bucket" {
+  value = module.bronze_bucket.bucket_name
+}
+
+output "silver_bucket" {
+  value = module.silver_bucket.bucket_name
+}
+
+output "gold_bucket" {
+  value = module.gold_bucket.bucket_name
+}
