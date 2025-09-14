000af7f2c0cc1c84cb9c7ea3b03ad12493726257
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-13 21:48:19 -0300

    feat: simplify Terraform provider configuration and set static backend values

--- provider.tf
+++ provider.tf
@@ -1,16 +1,14 @@
 terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.56"
-    }
+  backend "s3" {
+    bucket         = "prbr25-dev-ploiw-tfstate"   # replace with the actual bucket name created in bootstrap
+    key            = "infra/root.tfstate"
+    region         = "sa-east-1"                          # same region you used in bootstrap
+    dynamodb_table = "prbr25-dev-tf-locks"                # from bootstrap output
+    encrypt        = true
   }
-  # Leave values empty; we pass them via -backend-config on tofu init
-  backend "s3" {}
 }
 
 provider "aws" {
-  region  = var.region
-  profile = var.profile
+  region = "sa-east-1"
+  s3_use_path_style = true
 }
