c5135ac792feca6f0e5f159d70c10a17f7b1e983
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-13 22:50:38 -0300

    feat: add Glue catalog database and tables for S3 buckets

--- main.tf
+++ main.tf
@@ -21,3 +21,30 @@ module "gold_bucket" {
   unique_suffix = var.unique_suffix
   name          = "gold"
 }
+
+resource "aws_glue_catalog_database" "prbr25" {
+  name        = "${var.project}_${var.env}"
+  description = "Glue database for ${var.project} Iceberg tables"
+}
+
+# Glue tables for each S3 bucket
+module "glue_bronze" {
+  source        = "./modules/glue_table"
+  database_name = aws_glue_catalog_database.prbr25.name
+  table_name    = "bronze"
+  s3_path       = module.bronze_bucket.bucket_name
+}
+
+module "glue_silver" {
+  source        = "./modules/glue_table"
+  database_name = aws_glue_catalog_database.prbr25.name
+  table_name    = "silver"
+  s3_path       = module.silver_bucket.bucket_name
+}
+
+module "glue_gold" {
+  source        = "./modules/glue_table"
+  database_name = aws_glue_catalog_database.prbr25.name
+  table_name    = "gold"
+  s3_path       = module.gold_bucket.bucket_name
+}
